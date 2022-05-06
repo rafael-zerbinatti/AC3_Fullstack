4ee2402b4ccf926154125051306f7473159f1f97
rafaturbi <86134067+rafaturbi@users.noreply.github.com>
2022-05-06 20:45:37 -0300

    Update models.py

--- models.py
+++ models.py
@@ -11,9 +11,9 @@ class Produto(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String, nullable=False)
     categoria = db.Column(db.String, nullable=False)
-    preco = db.Column(db.String, nullable=False)
+    preco = db.Column(db.Float, nullable=False)
 
     def __init__(self, nome, categoria, preco):
         self.nome = nome
         self.categoria = categoria
-        self.preco = float(preco)
+        self.preco = preco
